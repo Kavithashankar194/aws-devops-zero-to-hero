01b1923b39868559a68ef9e98ca113a405222170
Kavithashankar194 <146877041+Kavithashankar194@users.noreply.github.com>
2023-10-15 00:35:13 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,6 +4,7 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull kavitha26/simple-python-flask-app
 
+
 # Run the Docker image as a container
 docker run -d -p 5000:5000 kavitha26/simple-python-flask-app
 
